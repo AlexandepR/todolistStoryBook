7a572c3d63b78b8d9f17c2aabec0a4c257777653
Alex <alexandepp@gmail.com>
2021-12-28 22:50:23 +0300

    todolist 15 p2 storybook demo default todolist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,9 @@
 import { tasksReducer } from '../features/todolistsList/Todolist/tasks-reducer';
 import { todolistsReducer } from '../features/todolistsList/Todolist/todolists-reducer';
 import {applyMiddleware, combineReducers, createStore} from 'redux';
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
 import {appReducer} from "./app-reducer";
+import thunkMiddleware from "redux-thunk";
 
 
 const rootReducer = combineReducers({
@@ -12,7 +13,7 @@ const rootReducer = combineReducers({
 })
 
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
